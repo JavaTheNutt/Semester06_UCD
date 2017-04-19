61c3472fe1ece418d0f264cfa1a0cfccf2336cf2
Joe Wemyss <joewemyss3@gmail.com>
2017-04-19 16:56:16 +0100

    added new sidebar

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,16 +1,19 @@
 <template>
-	<div class="container-fluid"
-		 v-bind:class="{noConstruction : !showUnderConstruction, construction: showUnderConstruction}">
-		<div v-bind:class="{'col-xs-1': isHideNav(), 'col-xs-3': isShowNav()}" class="affix mainBodyHolder">
-			<div class="col-xs-9" v-bind:class="{hidden : isHideNav()}">
-				<navigation-sidebar></navigation-sidebar>
-			</div>
-			<div v-bind:class="{'col-xs-3': isShowNav()}">
-				<button class="btn btn-danger" v-bind:class="{hidden : !hasNavLinks}" @click="toggleNav">{{toggleNavLabel}} navigation</button>
-			</div>
+	<div class="container-fluid" id="wrapper"
+		 v-bind:class="{noConstruction : !showUnderConstruction, construction: showUnderConstruction, toggled: navLinksManuallyShown}">
+		<div id="sidebar-wrapper">
+			<navigation-sidebar></navigation-sidebar>
 		</div>
-		<div class="mainBodyHolder" v-bind:class="{'col-xs-8' : isShowNav(), 'col-xs-10' : isHideNav(), 'col-xs-offset-1': hasNavLinks}">
-			<router-view></router-view>
+		<div class="mainBodyHolder" id="page-content-wrapper">
+			<div class="col-xs-1">
+				<button class="btn btn-danger affix" v-bind:class="{hidden : !hasNavLinks}"
+						@click="navLinksManuallyShown = !navLinksManuallyShown"><i class="fa"
+																				   v-bind:class="{'fa-arrow-left' : navLinksManuallyShown, 'fa-arrow-right': !navLinksManuallyShown}"></i>
+				</button>
+			</div>
+			<div v-bind:class="{'col-xs-11' : hasNavLinks, 'col-xs-12': !hasNavLinks}">
+				<router-view></router-view>
+			</div>
 		</div>
 
 	</div>
@@ -39,32 +42,19 @@
 		created(){
 			this.screenResize(true);
 			this.$log.debug("showNav", this.showNav);
-			bus.$on('set-nav-links', navLinks => this.hasNavLinks = navLinks !== null);
+			bus.$on('set-nav-links', navLinks => this.hasNavLinks = this.navLinksManuallyShown = navLinks !== null);
 			mediaWatch.$on('screen-resized', () => this.screenResize())
 		},
 		methods: {
 			screenResize(isInitial){
 				let showNav = this.$mq.above(this.$mv.sm);
-				if(isInitial){
+				if (isInitial) {
 					this.navLinksManuallyShown = showNav;
 				}
 				this.$log.debug('resize listener called for main section');
 				this.showNav = showNav;
-				this.setNavLabel();
-			},
-			isShowNav(){
-				return (this.showNav && this.hasNavLinks && this.navLinksManuallyShown);
-			},
-			isHideNav(){
-				return (!this.hasNavLinks || !this.navLinksManuallyShown);
-			},
-			toggleNav(){
-				this.navLinksManuallyShown = !this.navLinksManuallyShown;
-				this.setNavLabel();
-			},
-			setNavLabel(){
-				this.toggleNavLabel = this.isShowNav() ? 'Hide': "Show";
 			}
+
 		}
 
 	}
@@ -77,14 +67,87 @@
 	.noConstruction {
 		margin-top: 100px;
 	}
-	.navHidden{
+
+	.navHidden {
 		padding: 20px;
 	}
-	.sideNavHolder{
+
+	.sideNavHolder {
 		position: fixed;
 		float: left;
 	}
-	.mainBodyHolder{
+
+	.mainBodyHolder {
 		overflow: hidden;
+	}
+
+	#wrapper {
+		padding-left: 0;
+		-webkit-transition: all 0.5s ease;
+		-moz-transition: all 0.5s ease;
+		-o-transition: all 0.5s ease;
+		transition: all 0.5s ease;
+	}
+
+	#wrapper.toggled {
+		padding-left: 250px;
+	}
+
+	#sidebar-wrapper {
+		z-index: 1000;
+		position: fixed;
+		left: 250px;
+		width: 0;
+		height: 100%;
+		margin-left: -250px;
+		overflow-y: auto;
+		background: #000;
+		-webkit-transition: all 0.5s ease;
+		-moz-transition: all 0.5s ease;
+		-o-transition: all 0.5s ease;
+		transition: all 0.5s ease;
+	}
+
+	#wrapper.toggled #sidebar-wrapper {
+		width: 250px;
+	}
+
+	#page-content-wrapper {
+		width: 100%;
+		position: absolute;
+		padding: 15px;
+	}
+
+	#wrapper.toggled #page-content-wrapper {
+		position: absolute;
+		margin-right: -250px;
+	}
+
+	@media (min-width: 768px) {
+		#wrapper {
+			padding-left: 0;
+		}
+
+		#wrapper.toggled {
+			padding-left: 250px;
+		}
+
+		#sidebar-wrapper {
+			width: 0;
+		}
+
+		#wrapper.toggled #sidebar-wrapper {
+			width: 250px;
+		}
+
+		#page-content-wrapper {
+			padding: 20px;
+			position: relative;
+		}
+
+		#wrapper.toggled #page-content-wrapper {
+			position: relative;
+			margin-right: 0;
+		}
 	}
 </style>
